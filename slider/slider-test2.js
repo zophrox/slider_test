60de76bc0ba5abf8fbb44242cc2e70100bdd02dd
zophrox <zophrox@gmail.com>
2023-09-13 17:04:46 +0300

    added setInterval for auto-scrolling

--- slider/slider-test2.js
+++ slider/slider-test2.js
@@ -2,52 +2,57 @@ let item = document.querySelectorAll(".images-slideshow .imageSlides");
 let next = document.querySelector(".next");
 let previous = document.querySelector(".previous");
 
-let array = [];
+let indexItem = [];
 for (let i = 0; i < item.length; i++) {
-  array.push(i);
+  indexItem.push(i);
 }
 
-let active = Math.floor(array.length / 2);
+let active = Math.floor(indexItem.length / 2);
 function loadShow() {
   let stt = 0;
-  item[array[active]].style.transform = `none`;
-  item[array[active]].style.zIndex = 1;
-  item[array[active]].style.filter = "none";
-  item[array[active]].style.opacity = 1;
-  item[array[active]].style.transition = "all 0.3s 0.2s ease-out";
-  console.log(item[array[active - 1]]);
+  item[indexItem[active]].style.transform = `none`;
+  item[indexItem[active]].style.zIndex = 1;
+  item[indexItem[active]].style.filter = "none";
+  item[indexItem[active]].style.opacity = 1;
+  item[indexItem[active]].style.transition = "all 0.9s 0.1s ease-out";
+
   for (let i = active + 1; i < item.length; i++) {
     stt++;
-    item[array[i]].style.transform = `translateX(${200 * stt}px) scale(${
+    item[indexItem[i]].style.transform = `translateX(${200 * stt}px) scale(${
       0.8 - 0.2 * stt
     }) perspective(16px) rotateY(-1deg)`;
-    item[array[i]].style.zIndex = -stt;
-    item[array[i]].style.filter = "blur(5px)";
-    item[array[i]].style.opacity = stt > 2 ? 0.2 : 0.6;
-    item[array[i]].style.transition = "all 0.4s 0.1s ease-out";
+    item[indexItem[i]].style.zIndex = -stt;
+    item[indexItem[i]].style.filter = "blur(5px)";
+    item[indexItem[i]].style.opacity = stt > 2 ? 0.03 : 0.6;
+    item[indexItem[i]].style.transition = "all 0.9s 0.1s ease-out";
   }
   stt = 0;
   for (let i = active - 1; i >= 0; i--) {
     stt++;
-    item[array[i]].style.transform = `translateX(${-200 * stt}px) scale(${
+    item[indexItem[i]].style.transform = `translateX(${-200 * stt}px) scale(${
       0.8 - 0.2 * stt
     }) perspective(16px) rotateY(1deg)`;
-    item[array[i]].style.zIndex = -stt;
-    item[array[i]].style.filter = "blur(5px)";
-    item[array[i]].style.opacity = stt > 2 ? 0.2 : 0.6;
-    item[array[i]].style.transition = "all 0.4s 0.1s  ease-out";
+    item[indexItem[i]].style.zIndex = -stt;
+    item[indexItem[i]].style.filter = "blur(5px)";
+    item[indexItem[i]].style.opacity = stt > 2 ? 0.03 : 0.6;
+    item[indexItem[i]].style.transition = "all 0.9s 0.1s  ease-out";
   }
 }
 
 loadShow();
-
-next.onclick = function setSlides() {
-  let lastImg = array.pop();
-  array.unshift(lastImg);
+setInterval(() => {
+  let lastImg = indexItem.pop();
+  indexItem.unshift(lastImg);
   loadShow();
+}, 7000);
+next.onclick = () => {
+  let lastImg = indexItem.pop();
+  indexItem.unshift(lastImg);
+  loadShow();
+  console.log(indexItem);
 };
-previous.onclick = function setSlides() {
-  let lastImg = array.shift();
-  array.push(lastImg);
+previous.onclick = () => {
+  let lastImg = indexItem.shift();
+  indexItem.push(lastImg);
   loadShow();
 };
